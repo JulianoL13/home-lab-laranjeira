5b505e0358539a4ac53018c56f33d71c8ceb6113
JulianoL13 <125844980+JulianoL13@users.noreply.github.com>
2025-09-13 16:42:22 -0300

    Switch Proxmox provider to bpg

---
+++ terraform/variables.tf
@@ -0,0 +1,40 @@
+variable "pm_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+}
+
+variable "pm_user" {
+  description = "Proxmox user"
+  type        = string
+}
+
+variable "pm_password" {
+  description = "Proxmox password"
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "pm_api_token_id" {
+  description = "Proxmox API token ID"
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox API token secret"
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "cluster_name" {
+  description = "Proxmox cluster name"
+  type        = string
+}
+
+variable "template_name" {
+  description = "VM template name"
+  type        = string
+}
